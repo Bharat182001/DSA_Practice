872934fbb0929f27e1773996eaebe8b8af3844ad
Bharat <85057603+Bharat182001@users.noreply.github.com>
2023-03-07 17:17:17 +0530

    binary search nice problem

---
+++ 2187. Minimum time to Complete Trips.cpp
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/minimum-time-to-complete-trips/description/
+
+// 2187. Minimum Time to Complete Trips
+// LOGIC: brute is easy enough, optimised solution needs binary search and lower and upper limits are pretty much the cruz of problem.
+
+class Solution {
+public:
+    bool possibleHai(vector<int> &time, long long givenTime, long long totalTrips){
+        long long actualTrips=0;
+        for(auto &it: time){
+            actualTrips += (givenTime/it);
+        }
+        return (actualTrips >= totalTrips);
+    }
+    long long minimumTime(vector<int>& time, int totalTrips) {
+        // Best Solution, TC: O(nlog(p)), p -> min_ele*totalTrips
+        int n=time.size();
+
+        long long l=1;
+        long long h=(long long) *min_element(begin(time), end(time)) * totalTrips;
+
+        while(l<h){
+            long long mid = l + (h-l)/2;
+
+            if(possibleHai(time, mid, totalTrips)){
+                h=mid;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return l;
+    }
+};
