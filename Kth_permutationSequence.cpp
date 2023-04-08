a06c7ecdfb272cf189f56e1bed4d99d4ee09f7d1
Bharat Data <85057603+Bharat182001@users.noreply.github.com>
2023-04-09 01:30:36 +0530

    LeetCode 60
    Permutation Sequence

--- Kth_permutationSequence.cpp
+++ Kth_permutationSequence.cpp
@@ -18,3 +18,34 @@ public:
         return str;
     }
 };
+
+// Approach 2: do by decreaing search set space given and placing the required index number to answer 
+
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        // TC: O(n*n), SC: O(n)
+        k--; // keeping 0-based indexing
+
+        vector<int> temp;
+        int fact=1;
+        for(int i=1; i<n; i++) { // run till n-1 so as to get fact value as n!/setSize
+            fact *= i;
+            temp.push_back(i);
+        }
+        temp.push_back(n);
+
+        string res="";
+        while(true) {
+            res += (to_string(temp[k/fact]));
+            temp.erase(temp.begin() + (k/fact));
+
+            if(temp.size()==0) break;
+
+            k = k%fact;
+            fact = fact/temp.size();
+        }
+
+        return res;
+    }
+};
