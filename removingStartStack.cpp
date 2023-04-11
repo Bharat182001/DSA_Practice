fbe1a9e070b626b368fc21eac8614bf7377b08a0
Bharat Data <85057603+Bharat182001@users.noreply.github.com>
2023-04-11 08:51:49 +0530

    LeetCode 2390
    Removing Stars From a String

---
+++ removingStartStack.cpp
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/removing-stars-from-a-string/description/
+
+// Approach: Could be done by stack easily, but two pointer approach for this problem is not easy to think!
+
+// Two Pointers Approach
+class Solution {
+public:
+    string removeStars(string s) {
+        vector<char> ch(s.size());
+        int j = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '*') {
+                j--;
+            } else {
+                ch[j++] = s[i];
+            }
+        }
+
+        string answer = "";
+        for (int i = 0; i < j; i++) {
+            answer.push_back(ch[i]);
+        }
+
+        return answer;
+    }
+};
