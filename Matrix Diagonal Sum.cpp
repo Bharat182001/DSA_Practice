6e363d88592702fe60139c5e642c70d682ca024e
Bharat Data <85057603+Bharat182001@users.noreply.github.com>
2023-05-08 11:55:53 +0530

    LeetCode 1572
    Matrix Diagonal Sum

---
+++ Matrix Diagonal Sum.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/matrix-diagonal-sum/description/
+
+// TC: O(n)
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n=mat.size();
+
+        int sum=0;
+        for(int i=0; i<n; i++) {
+            sum += (mat[i][i] + mat[i][n-i-1]);
+        }
+        
+        if(n&1) sum -= mat[n/2][n/2];
+
+        return sum;
+    }
+};
